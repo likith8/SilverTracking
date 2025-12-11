b5e1f9143d7c89989912707146871083e042bb48
Likith M <likithm539@gmail.com>
2025-12-11 16:53:42 +0530

    "DatabaseConnect"

--- silvertrack/settings.py
+++ silvertrack/settings.py
@@ -82,8 +82,15 @@ WSGI_APPLICATION = 'silvertrack.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LIKITHMADHU$SilverTracking',
+        'USER': 'LIKITHMADHU',
+        'PASSWORD': 'Madhu@2004',
+        'HOST': 'LIKITHMADHU.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
